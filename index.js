320d8d1beb40047535e4e2129b362644b62e57af
TatiTait <ladytatitait@gmail.com>
2021-09-02 12:06:41 +1000

    first commit

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express')
-const data = require('./data')
 const app = express()
-const PORT = 3000
-const message = "Gday, mate!"
-console.log(data)
+const data = require('./data')
+const PORT = process.env.PORT || 3000
 
-// Get request - route
+//Homepage
+
 app.get('/', (req, res) => {
-    res.send(message)
+    res.send("Welcome to our schedule website")
 })
 
-//POST request
+//Dysplay all users
+
+app.get('/users', (req, res) => {
+    res.json(data.users)
+})
+
+
+//Dysplay a new user
+
+app.post('/users', (req, res) => {
+    res.json(req.body)
+})
+
+//Dysplay a single schedule
+
+app.get('/schedules/:id', (req, res) => {
+    const found = data.schedules.some(schedule => schedule.id === Number(req.params.id))
+    
+    if (found) {
+        const schedule = data.schedules.filter(schedule => schedule.id === Number(req.params.id))
+    res.send(schedule[0])
+    } else {
+        res.send('Schedules not found')
+    }
+})
+
 
 app.listen(PORT, () => {
     console.log(`Gday, mate! Here's your app: http://localhost:${PORT}`)
