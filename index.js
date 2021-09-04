4cf3e9c32a1988e25d6b8cd319126974c5ed5821
TatiTait <ladytatitait@gmail.com>
2021-09-04 18:45:39 +1000

    Completed 3b with styles and working routes

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express()
 const data = require('./data')
 const PORT = process.env.PORT || 3000
 const bcrypt = require('bcryptjs');
+const expressLayouts = require('express-ejs-layouts')
 
 
 // body parser middleware
@@ -10,67 +11,19 @@ const bcrypt = require('bcryptjs');
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
-//Homepage
+// set up template engine
+app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    res.send("Welcome to our schedule website")
-})
-
-//All users
-
-app.get('/users', (req, res) => {
-    res.send(data.users)
-})
-
-//All schedules
-
-app.get("/schedules", (req, res) => {
-    res.send(data.schedules)
-})
+// set a layout
+app.use(expressLayouts)
+app.set("layout","layouts/layout.ejs")
 
 
-//Single user 
+// set up static folder
+app.use(express.static('public'))
 
-app.get("/users/:id", (req, res) => {
-    res.send (data.users[parseInt (req.params.id)])
-})
-
-//Schedules for single user 
-
-app.get("/users/:id/schedules", (req, res) => {
-    const id = parseInt(req.params.id)
-    singleSchedules = data.schedules.filter(schedule => schedule.user_id === id)
-    res.send(singleSchedules)
-})
-
-//Add new schedule for user in POST and return updated info
-
-app.post("/schedules", (req, res) => {
-    const newSchedule = req.body
-    data.schedules.push(newSchedule)
-    res.send(data.schedules)
-})
-
-
-//Add new user in POST and return updated users list, password encrypted 
-
-app.post('/users', (req, res) => {
-    const {firstname, lastname, email, password} = req.body
-    const salt = bcrypt.genSaltSync(10)
-    const hash = bcrypt.hashSync(password, salt)
-    //console.log(hash)
-    const newUser = {
-        firstname, lastname, email, password:hash
-    }
-    data.users.push(newUser)
-    res.json(data.users)
-})
-
-
-// For invalid routes
-app.get('*', (req, res) => {
-    res.send('404! This is an invalid URL.');
-  });
+// set up routes
+app.use("", require("./routes/routes"))
 
 
 app.listen(PORT, () => {
